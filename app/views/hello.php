b8c46c9a5be4ca784c1e0bb8caf02000ea11ae34
Chase Lee <clee231@uic.edu>
2014-05-09 01:03:37 -0500

    Implemented template in Framework for PHP. Not requires other packages. To be documented later...

---
+++ app/views/hello.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Linux Users Group 3D Printer System</title>
+	<link rel="stylesheet" href="css/vendor/f/foundation-icons.css" />
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.welcome {
+			width: 500px;
+			height: 200px;
+			position: absolute;
+			left: 50%;
+			top: 30%;
+			margin-left: -250px;
+			margin-top: -100px;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+		#placeholder {
+			font-size:15rem;
+			color: #FF2626;
+		}
+	</style>
+</head>
+<body>
+	<div class="welcome">
+		<a href="#" title="LUG 3d Print System FTW!"><span id="placeholder" class="fi-layout"></span></a>
+		<h1>LUG 3D Printer System</h1>
+		<h2>is under construction.</h2>
+		<h3>Please check back during the Fall 2014 semester.</h3>
+	</div>
+</body>
+</html>
---
+++ app/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+			<footer class="row">
+				<div class="large-12 columns"><hr />
+					<div id="mattidea" class="row">
+						<p class="footer-note">Matt's idea of a proper footer</p>
+						<img class="lug-footer-img" src="img/BlackTux.png" alt="Matt's idea of a proper footer"/><!-- Matt's idea -->
+					</div><!-- mattidea -->
+				</div><!-- large-12 columns -->
+			</footer>
